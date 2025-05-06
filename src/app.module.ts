1708a8be812478a2c4a435ce4793f8986154bcd5
João Paulo Azevedo <azevedo.jpf@gmail.com>
2025-05-06 09:22:07 -0300

    fix: organizing the main project directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { ComplaintsModule } from './complaints/complaints.module';
 @Module({
 
   imports: [
-    MongooseModule.forRoot('mongodb+srv://joaoazevedo:jgY8LYNmMBg9nI0r@poto.zqgwluj.mongodb.net/'),
+    MongooseModule.forRoot('mongodb://localhost:27017'),
     PostModule,
     CommentsModule,
     StudentsModule,
