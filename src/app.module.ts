15081e8083445aa7a355f174c444205cfe8fd1d3
João Paulo Azevedo <azevedo.jpf@gmail.com>
2025-11-27 23:40:31 -0300

    refactor: remove Auth and Mailer modules and related files

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,8 +10,6 @@ import { ComplaintsModule } from './complaints/complaints.module';
 import { LoginJwtModule } from './login-jwt/login-jwt.module';
 import { FormsModule } from './forms/forms.module';
 import { NewsModule } from './news/news.module';
-import { MailerModule } from './mailer/mailer.module';
-import { AuthModule } from './auth/auth.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { ResponsesModule } from './responses/responses.module';
 
@@ -36,8 +34,6 @@ import { ResponsesModule } from './responses/responses.module';
     LoginJwtModule,
     FormsModule,
     NewsModule,
-    MailerModule,
-    AuthModule,
     CloudinaryModule,
     ResponsesModule
   ],
